1d1b8125c401fc4db597160fca2978fd6f29f957
Samson Badejo <zwitsam8@gmail.com>
2025-09-08 22:48:50 +0100

    fix: configure CORS middleware with specific options

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://rccghouseofdavid.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
